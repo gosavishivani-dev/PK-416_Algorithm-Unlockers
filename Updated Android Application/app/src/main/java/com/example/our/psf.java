823c4d307e3b3a1c2d7459d526526f2bb0d26f93
vaishalipomnar53 <53169021+vaishalipomnar53@users.noreply.github.com>
2020-08-02 18:22:13 +0530

    Updated Android app GUI And Connectivity

---
+++ Updated Android Application/app/src/main/java/com/example/our/psf.java
@@ -0,0 +1,76 @@
+package com.example.our;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class psf extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_psf);
+
+
+        Spinner spinner1=findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.numbers,android.R.layout.simple_spinner_item);
+       adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner2=findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter1= ArrayAdapter.createFromResource(this,R.array.num,android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+       spinner1.setAdapter(adapter);
+       spinner1.setOnItemSelectedListener(this);
+       spinner2.setAdapter(adapter1);
+       spinner2.setOnItemSelectedListener(this);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.about);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.dashboard:
+                        startActivity(new Intent(getApplicationContext(),price_prediction.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),crop_arrival.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.about:
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
